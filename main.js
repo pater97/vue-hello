c560e42ff33529606170648f7ff454b91aec6eec
pater97 <giuseppepater997@gmail.com>
2021-10-28 13:30:24 +0200

    esercizio terminato

---
+++ main.js
@@ -0,0 +1,8 @@
+const main = new Vue({
+    el: `#root`,
+    data: {
+        message:`nike`,
+        titleClass:`title`,
+        link: `https://c.static-nike.com/a/images/w_1920,c_limit/mdbgldn6yg1gg88jomci/image.jpg`
+    }
+})
